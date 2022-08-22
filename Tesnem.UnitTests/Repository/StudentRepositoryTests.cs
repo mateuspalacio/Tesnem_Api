c8e07324705088b62ffb90c8fd8bb8ed3bdb126a
Mateus Palácio <mateuspalacio@gmail.com>
2022-08-22 14:51:05 -0300

    started adding

---
+++ Tesnem.UnitTests/Repository/StudentRepositoryTests.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tesnem.Api.Domain.Models;
+using Tesnem.Api.Domain.Repository;
+
+namespace Tesnem.UnitTests.Repository
+{
+    public class StudentRepositoryTests
+    {
+        private Mock<IStudentRepository> _rep = new Mock<IStudentRepository>();
+
+        [Fact]
+        public async Task Should_Add_Student()
+        {
+            var student = new Student()
+            {
+                Name = "a"
+            };
+
+            var resp = await _rep.Object.Add(student);
+
+            Assert.NotNull(resp);
+        }
+    }
+}
