0abba718a24585fdc265e6c279ab551fe28210f3
Mateus Palácio <mateuspalacio@gmail.com>
2022-08-22 15:41:02 -0300

    Added all repo tests

---
+++ Tesnem.UnitTests/Repository/ClassRepositoryTests.cs
@@ -0,0 +1,121 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tesnem.Api.Domain.Models;
+using Tesnem.Api.Domain.Repository;
+
+namespace Tesnem.UnitTests.Repository
+{
+    public class ClassRepositoryTests
+    {
+        private Mock<IClassRepository> _rep = new Mock<IClassRepository>();
+
+        [Fact]
+        public async Task Should_Add_Class()
+        {
+            // Arrange
+            var _class = new Mock<Class>();
+            _rep.Setup(x => x.Add(It.IsAny<Class>())).Returns(Task.FromResult(_class.Object));
+
+            // Act
+            var resp = await _rep.Object.Add(_class.Object);
+
+            // Assert
+            Assert.NotNull(resp);
+            Assert.Equal(_class.Object, resp);
+        }
+
+        [Fact]
+        public async Task Should_Get_Class()
+        {
+            // Arrange
+            var _class = new Mock<Class>();
+            _rep.Setup(x => x.GetById(It.IsAny<Guid>())).Returns(Task.FromResult(_class.Object));
+
+            // Act
+            var resp = await _rep.Object.GetById(_class.Object.Id);
+
+            // Assert
+            Assert.NotNull(resp);
+            Assert.Equal(_class.Object, resp);
+        }
+
+        [Fact]
+        public async Task Should_Get_Many_Classs()
+        {
+            // Arrange
+            var item = new Mock<Class>();
+            var _class = new List<Class>() { item.Object };
+            _rep.Setup(x => x.GetAllClasses()).Returns(Task.FromResult((IEnumerable<Class>)_class));
+
+            // Act
+            var resp = await _rep.Object.GetAllClasses();
+
+            // Assert
+            Assert.NotNull(resp);
+            Assert.Equal(_class, resp);
+        }
+        [Fact]
+        public async Task Should_Get_Many_Class_By_Course()
+        {
+            // Arrange
+            var _class = new Mock<Class>();
+            var courseId = Guid.NewGuid();
+            _rep.Setup(x => x.GetByCourseId(It.IsAny<Guid>())).Returns(Task.FromResult(_class.Object));
+
+            // Act
+            var resp = await _rep.Object.GetByCourseId(courseId);
+
+            // Assert
+            Assert.NotNull(resp);
+            Assert.Equal(_class.Object, resp);
+        }
+        [Fact]
+        public async Task Should_Get_Many_Class_By_Major()
+        {
+            // Arrange
+            var _class = new Mock<Class>();
+            var majorId = Guid.NewGuid();
+            _rep.Setup(x => x.GetByMajorId(It.IsAny<Guid>())).Returns(Task.FromResult(_class.Object));
+
+            // Act
+            var resp = await _rep.Object.GetByMajorId(majorId);
+
+            // Assert
+            Assert.NotNull(resp);
+            Assert.Equal(_class.Object, resp);
+        }
+
+        [Fact]
+        public void Should_Remove_Classs()
+        {
+            // Arrange
+            var _class = new Mock<Class>();
+            _rep.Setup(x => x.Delete(It.IsAny<Class>())).Returns(Task.FromResult(_class.Object));
+
+            // Act
+            var resp = _rep.Object.Delete(_class.Object).IsCompletedSuccessfully;
+
+            // Assert
+            Assert.True(resp);
+        }
+
+        [Fact]
+        public async Task Should_Update_Class()
+        {
+            // Arrange
+            var _class = new Mock<Class>();
+            _rep.Setup(x => x.Update(It.IsAny<Class>())).Returns(Task.FromResult(_class.Object));
+
+            // Act
+            var resp = await _rep.Object.Update(_class.Object);
+
+            // Assert
+            Assert.NotNull(resp);
+            Assert.Equal(_class.Object, resp);
+        }
+    }
+}
--- Tesnem.UnitTests/Repository/StudentRepositoryTests.cs
+++ Tesnem.UnitTests/Repository/StudentRepositoryTests.cs
@@ -16,14 +16,127 @@ namespace Tesnem.UnitTests.Repository
         [Fact]
         public async Task Should_Add_Student()
         {
-            var student = new Student()
-            {
-                Name = "a"
-            };
+            // Arrange
+            var student = new Mock<Student>();
+            _rep.Setup(x => x.Add(It.IsAny<Student>())).Returns(Task.FromResult(student.Object));
 
-            var resp = await _rep.Object.Add(student);
-
+            // Act
+            var resp = await _rep.Object.Add(student.Object);
+            
+            // Assert
             Assert.NotNull(resp);
+            Assert.Equal(student.Object, resp);
+        }
+
+        [Fact]
+        public async Task Should_Get_Student()
+        {
+            // Arrange
+            var student = new Mock<Student>();
+            _rep.Setup(x => x.GetById(It.IsAny<Guid>())).Returns(Task.FromResult(student.Object));
+
+            // Act
+            var resp = await _rep.Object.GetById(student.Object.Id);
+
+            // Assert
+            Assert.NotNull(resp);
+            Assert.Equal(student.Object, resp);
+        }
+
+        [Fact]
+        public async Task Should_Get_Many_Students()
+        {
+            // Arrange
+            var item = new Mock<Student>();
+            var student = new List<Student>() { item.Object };
+            _rep.Setup(x => x.GetAllStudents()).Returns(Task.FromResult((IEnumerable<Student>)student));
+
+            // Act
+            var resp = await _rep.Object.GetAllStudents();
+
+            // Assert
+            Assert.NotNull(resp);
+            Assert.Equal(student, resp);
+        }
+
+        [Fact]
+        public async Task Should_Get_Many_Students_By_Course()
+        {
+            // Arrange
+            var item = new Mock<Student>();
+            var student = new List<Student>() { item.Object };
+            var courseId = Guid.NewGuid();
+            _rep.Setup(x => x.GetAllStudentsByCourse(It.IsAny<Guid>())).Returns(Task.FromResult((IEnumerable<Student>)student));
+
+            // Act
+            var resp = await _rep.Object.GetAllStudentsByCourse(courseId);
+
+            // Assert
+            Assert.NotNull(resp);
+            Assert.Equal(student, resp);
+        }
+
+        [Fact]
+        public async Task Should_Get_Many_Students_By_Class()
+        {
+            // Arrange
+            var item = new Mock<Student>();
+            var student = new List<Student>() { item.Object };
+            var classId = Guid.NewGuid();
+            _rep.Setup(x => x.GetAllStudentsByClass(It.IsAny<Guid>())).Returns(Task.FromResult((IEnumerable<Student>)student));
+
+            // Act
+            var resp = await _rep.Object.GetAllStudentsByClass(classId);
+
+            // Assert
+            Assert.NotNull(resp);
+            Assert.Equal(student, resp);
+        }
+
+        [Fact]
+        public async Task Should_Get_Many_Students_By_Major()
+        {
+            // Arrange
+            var item = new Mock<Student>();
+            var student = new List<Student>() { item.Object };
+            var majorId = Guid.NewGuid();
+            _rep.Setup(x => x.GetAllStudentsByMajor(It.IsAny<Guid>())).Returns(Task.FromResult((IEnumerable<Student>)student));
+
+            // Act
+            var resp = await _rep.Object.GetAllStudentsByMajor(majorId);
+
+            // Assert
+            Assert.NotNull(resp);
+            Assert.Equal(student, resp);
+        }
+
+        [Fact]
+        public void Should_Remove_Students()
+        {
+            // Arrange
+            var student = new Mock<Student>();
+            _rep.Setup(x => x.Delete(It.IsAny<Student>())).Returns(Task.FromResult(student.Object));
+
+            // Act
+            var resp = _rep.Object.Delete(student.Object).IsCompletedSuccessfully;
+
+            // Assert
+            Assert.True(resp);
+        }
+
+        [Fact]
+        public async Task Should_Update_Student()
+        {
+            // Arrange
+            var student = new Mock<Student>();
+            _rep.Setup(x => x.Update(It.IsAny<Student>())).Returns(Task.FromResult(student.Object));
+
+            // Act
+            var resp = await _rep.Object.Update(student.Object);
+
+            // Assert
+            Assert.NotNull(resp);
+            Assert.Equal(student.Object, resp);
         }
     }
 }
